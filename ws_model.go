62b7a33f16885b042d3bbd01f9cb7d188a7fd4e5
Ryan Nitz <ryan.nitz@coinbase.com>
2024-10-09 12:40:30 -0400

    Name changes

---
+++ ws_model.go
@@ -0,0 +1,91 @@
+/**
+ * Copyright 2024-present Coinbase Global, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package prime
+
+type WebSocketSubscribeMessage struct {
+	Type         string   `json:"type"`
+	Channel      string   `json:"channel"`
+	AccessKey    string   `json:"access_key"`
+	SvcAccountId string   `json:"api_key_id"`
+	Timestamp    string   `json:"timestamp"`
+	Passphrase   string   `json:"passphrase"`
+	Signature    string   `json:"signature"`
+	PortfolioId  string   `json:"portfolio_id,omitempty"`
+	ProductIds   []string `json:"product_ids,omitempty"`
+}
+
+type WebSocketUnsubscribeMessage struct {
+	Type     string   `json:"type"`
+	Channels []string `json:"channels"`
+}
+
+type WebSocketHeartbeatMessage struct {
+	Channel     string                     `json:"channel"`
+	Timestamp   string                     `json:"timestamp"`
+	SequenceNum int                        `json:"sequence_num"`
+	Events      []*WebSocketHeartbeatEvent `json:"events"`
+}
+
+type WebSocketHeartbeatEvent struct {
+	Subscriptions *WebSocketHeartbeatEventSubscriptions `json:"subscriptions"`
+}
+
+type WebSocketHeartbeatEventSubscriptions struct {
+	Heartbeats []string `json:"heartbeats"`
+}
+
+type WebSocketOrdersMessage struct {
+	Channel     string                  `json:"channel"`
+	Timestamp   string                  `json:"timestamp"`
+	SequenceNum int                     `json:"sequence_num"`
+	Events      []*WebSocketOrdersEvent `json:"events"`
+}
+
+type WebSocketOrdersEvent struct {
+	Type   string            `json:"type"`
+	Orders []*WebSocketOrder `json:"orders"`
+}
+
+type WebSocketOrder struct {
+	OrderId            string `json:"order_id"`
+	ClientOrderId      string `json:"client_order_id"`
+	CumulativeQuantity string `json:"cum_qty"`
+	LeavesQuantity     string `json:"leaves_qty"`
+	AveragePrice       string `json:"avg_px"`
+	Fees               string `json:"fees"`
+	Status             string `json:"status"`
+}
+
+type WebSocketL2DataMessage struct {
+	Channel     string                  `json:"channel"`
+	Timestamp   string                  `json:"timestamp"`
+	SequenceNum int                     `json:"sequence_num"`
+	Events      []*WebSocketL2DataEvent `json:"events"`
+}
+
+type WebSocketL2DataEvent struct {
+	Type      string                   `json:"type"`
+	ProductId string                   `json:"product_id"`
+	Updates   []*WebSocketL2DataUpdate `json:"updates"`
+}
+
+type WebSocketL2DataUpdate struct {
+	Side      string `json:"side"`
+	EventTime string `json:"event_time"`
+	Price     string `json:"px"`
+	Quantity  string `json:"qty"`
+}
