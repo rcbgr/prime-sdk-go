2e6de25b2abe5ae36dfab788e1323ae4d37f81a7
Ryan Nitz <ryan.nitz@coinbase.com>
2024-10-23 11:41:58 -0400

    Initial

---
+++ list_onchain_wallet_balances.go
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2024-present Coinbase Global, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package prime
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/coinbase-samples/core-go"
+)
+
+type ListOnchainWalletBalancesRequest struct {
+	PortfolioId         string            `json:"portfolio_id"`
+	WalletId            string            `json:"wallet_id"`
+	VisiblilityStatuses []string          `json:"visibility_statuses"`
+	Pagination          *PaginationParams `json:"pagination_params"`
+}
+
+type ListOnchainWalletBalancesResponse struct {
+	Balances              []*Balance                        `json:"balances"`
+	Type                  string                            `json:"type"`
+	TradingWalletBalances *BalanceWithHolds                 `json:"trading_balances"`
+	VaultWalletBalances   *BalanceWithHolds                 `json:"vault_balances"`
+	Request               *ListOnchainWalletBalancesRequest `json:"request"`
+}
+
+func (c *ClientImpl) ListOnchainWalletBalances(
+	ctx context.Context,
+	request *ListOnchainWalletBalancesRequest,
+) (*ListOnchainWalletBalancesResponse, error) {
+
+	path := fmt.Sprintf(
+		"/portfolios/$s/wallets/%s/web3_balances",
+		request.PortfolioId,
+		request.WalletId,
+	)
+
+	var queryParams string
+
+	queryParams = appendPaginationParams(queryParams, request.Pagination)
+
+	for _, v := range request.VisiblilityStatuses {
+		queryParams = appendQueryParam(queryParams, "visibility_statuses", v)
+	}
+
+	response := &ListOnchainWalletBalancesResponse{Request: request}
+
+	if err := core.Get(ctx, c, path, queryParams, request, response, c.headersFunc); err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
---
+++ list_portfolio_fills.go
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2024-present Coinbase Global, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package prime
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/coinbase-samples/core-go"
+)
+
+type ListPortfolioFillsRequest struct {
+	PortfolioId string            `json:"portfolio_id"` // required
+	Start       time.Time         `json:"start_date"`   // required
+	End         time.Time         `json:"end_date"`     // required
+	Pagination  *PaginationParams `json:"pagination_params"`
+}
+
+type ListPortfolioFillsResponse struct {
+	Fills      []*OrderFill               `json:"fills"`
+	Pagination *Pagination                `json:"pagination"`
+	Request    *ListPortfolioFillsRequest `json:"request"`
+}
+
+func (c *ClientImpl) ListPortfolioFills(
+	ctx context.Context,
+	request *ListPortfolioFillsRequest,
+) (*ListPortfolioFillsResponse, error) {
+
+	path := fmt.Sprintf("/portfolios/%s/fills", request.PortfolioId)
+
+	queryParams := appendPaginationParams(core.EmptyQueryParams, request.Pagination)
+
+	queryParams = appendQueryParam(queryParams, "start_date", TimeToStr(request.Start))
+
+	if !request.End.IsZero() {
+		queryParams = appendQueryParam(queryParams, "end_datae", TimeToStr(request.End))
+	}
+
+	response := &ListPortfolioFillsResponse{Request: request}
+
+	if err := core.Get(ctx, c, path, queryParams, request, response, c.headersFunc); err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
